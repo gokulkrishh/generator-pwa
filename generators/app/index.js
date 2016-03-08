e93527b13565c3310f300139020883a07c301d12
gokulkrishh <krishnangokul9@gmail.com>
2016-03-08 16:20:59 +0530

    Updated

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,7 +1,18 @@
 'use strict';
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
+var updateNotifier = require('update-notifier');
 var yosay = require('yosay');
+var _ = require('underscore.string');
+var path = require('path');
+var pkg = require('../../package.json');
+var notifier = updateNotifier({pkg: pkg});
+
+//If an update for generator avaiable, notify user
+if (notifier.update) {
+  notifier.notify();
+  console.log(notifier.update); //Info about the update
+}
 
 module.exports = yeoman.generators.Base.extend({
   prompting: function () {
@@ -12,11 +23,22 @@ module.exports = yeoman.generators.Base.extend({
       'Welcome to the peachy ' + chalk.red('Progresssive Web App') + ' generator!'
     ));
 
+    this.log(
+      chalk.yellow('┌──────────────────────────────────────────────────────────────┐ \n' +
+                   '| Answer few questions to kick start your pwa application      | \n' +
+                   '└──────────────────────────────────────────────────────────────┘ ')
+    )
+
     //No prompts as of now.
     var prompts = [{
+      name: 'appName',
+      message: 'What would you like to name your app?',
+      default: process.cwd().split(path.sep).pop()
+    },
+    {
       type: 'confirm',
       name: 'pushNotification',
-      message: 'Would you like to add push notification ?',
+      message: 'Would you like to add push notification?',
       default: true
     },
     {
@@ -56,12 +78,9 @@ module.exports = yeoman.generators.Base.extend({
 
     this.prompt(prompts, function (props) {
       this.props = props;
-      console.log('props --->', props);
 
-      if (this.props.pushNotification) {
-        console.log(this.props.apiKey);
-        this.apiKey = this.props.apiKey;
-      }
+      this.appName = _.camelize(_.slugify(_.humanize(props.appName)));
+
       // To access props later use this.props.someOption;
       done();
     }.bind(this));
@@ -70,60 +89,66 @@ module.exports = yeoman.generators.Base.extend({
   writing: function () {
     this.fs.copy(
       this.templatePath('css'),
-      this.destinationPath('css')
+      this.destinationPath(this.appName + '/css')
     );
     this.fs.copy(
       this.templatePath('images'),
-      this.destinationPath('images')
+      this.destinationPath(this.appName + '/images')
     );
     this.fs.copyTpl(
       this.templatePath('js/app.js'),
-      this.destinationPath('js/app.js'),
+      this.destinationPath(this.appName + '/js/app.js'),
       { pushNotification: this.props.pushNotification }
     );
     this.fs.copy(
       this.templatePath('favicon.ico'),
-      this.destinationPath('favicon')
+      this.destinationPath(this.appName + '/favicon')
     );
     this.fs.copy(
       this.templatePath('index.html'),
-      this.destinationPath('index.html')
+      this.destinationPath(this.appName + '/index.html')
     );
     this.fs.copy(
       this.templatePath('sw.js'),
-      this.destinationPath('sw.js')
+      this.destinationPath(this.appName + '/sw.js')
     );
     this.fs.copy(
       this.templatePath('sw-cache-polyfill.js'),
-      this.destinationPath('sw-cache-polyfill.js')
+      this.destinationPath(this.appName + '/sw-cache-polyfill.js')
     );
     this.fs.copyTpl(
       this.templatePath('manifest.json'),
-      this.destinationPath('manifest.json'),
+      this.destinationPath(this.appName + '/manifest.json'),
       { gcmSenderId: this.props.gcmSenderId }
     );
     this.fs.copy(
       this.templatePath('package.json'),
-      this.destinationPath('package.json')
+      this.destinationPath(this.appName + '/package.json')
     );
 
     //If push notifications is prompted
     if (this.props.pushNotification) {
       this.fs.copyTpl(
         this.templatePath('server.js'),
-        this.destinationPath('server.js'),
+        this.destinationPath(this.appName + '/server.js'),
         { apiKey: this.props.apiKey }
       );
 
       this.fs.copyTpl(
         this.templatePath('js/push.js'),
-        this.destinationPath('js/push.js'),
+        this.destinationPath(this.appName + '/js/push.js'),
         { apiKey: this.props.apiKey }
       );
     }
   },
 
   install: function () {
+    this.log(
+      chalk.green('\n ✔  Project structure created successfully! \n\n') +
+      chalk.yellow('┌──────────────────────────────────────────────────────────────┐ \n' +
+                   '| Installating Dependencies, Please wait...                    | \n' +
+                   '└──────────────────────────────────────────────────────────────┘ ')
+    );
     this.installDependencies();
   }
 });
